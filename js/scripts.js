ecbc021d3ddb2a54fe8b06dae64ff617f917b800
Kyle Harrington and Zach Weintraub <zach@>
2019-03-27 16:36:56 -0700

    add reciept viewer - name/address only

--- js/scripts.js
+++ js/scripts.js
@@ -12,6 +12,42 @@ $(function() {
     };
   });
 
+  $('#sameaddress').change(function(){
+    if($('#sameaddress').is(":checked")) {
+      $('#shippingaddress1').val($('#address1').val());
+      $('#shippingaddress2').val($('#address2').val());
+      $('#shippingcity').val($('#city').val());
+      $('#shippingstates').val($('#states').val());
+      $('#shippingzip').val($('#zip').val());
+      $('#shippingaddress1').attr('disabled', 'disabled');
+      $('#shippingaddress2').attr('disabled', 'disabled');
+      $('#shippingcity').attr('disabled', 'disabled');
+      $('#shippingstates').attr('disabled', 'disabled');
+      $('#shippingzip').attr('disabled', 'disabled');
+    } else {
+   $('#shippingaddress1').removeAttr('disabled');
+   $('#shippingaddress2').removeAttr('disabled');
+   $('#shippingcity').removeAttr('disabled');
+   $('#shippingstates').removeAttr('disabled');
+   $('#shippingzip').removeAttr('disabled');
+    }
+
+  });
+
+  $('#submit').click(function(event){
+    console.log('working');
+    $('#receipt-name').text($('#first-name').val() + ' ' + $('#last-name').val());
+    $('#receipt-address').text($('#address1').val());
+    $('#receipt-address2').text($('#address2').val());
+    $('#receipt-city').text($('#city').val());
+    $('#receipt-states').text($('#states').val());
+    $('#receipt-zip').text($('#zip').val());
+    $('#checkout-form').hide();
+    $('#receipt').slideDown();
+    event.preventDefault();
+
+  });
+
 
 
 
